1c56607eb43363af8323aa61f4e5d3f36ccbc632
Tom Kaeppler <tomkaeppler@web.de>
2022-11-08 15:38:48 +0100

    start adding configs and run prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,11 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  arrowParens: 'always',
+  printWidth: 100,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: 'all',
+  tabWidth: 2,
 };
