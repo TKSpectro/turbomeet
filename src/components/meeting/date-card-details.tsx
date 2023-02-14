9ce863541bf1ef8e606993751d256528a4ed3ceb
Tom Kaeppler <tomkaeppler@web.de>
2023-02-14 16:06:31 +0100

    refactor date card with appointment times

---
+++ src/components/meeting/date-card-details.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import DateCard from './date-card';
+
+export interface DateCardDetailsProps {
+  value: string;
+  className?: string;
+}
+
+const DateCardDetails: React.FunctionComponent<DateCardDetailsProps> = ({ value, className }) => {
+  const start = new Date(value.split('/')[0] || '');
+  const endDate = value.split('/')[1];
+  const end = endDate ? new Date(endDate) : undefined;
+
+  return (
+    <>
+      <DateCard date={start} />
+      {end && (
+        <div>
+          <div className="relative -mr-2 inline-block pr-2 text-right text-xs font-semibold after:absolute after:top-2 after:right-0 after:h-4 after:w-1 after:border-t after:border-r after:border-b after:border-slate-300 after:content-['']">
+            <div>{start.toLocaleTimeString()}</div>
+            <div className="text-slate-400">{end.toLocaleTimeString()}</div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default DateCardDetails;
--- src/components/pages/meeting-detail.tsx
+++ src/components/pages/meeting-detail.tsx
@@ -15,7 +15,7 @@ import { useState } from 'react';
 import { useCopyToClipboard } from 'react-use';
 import type { RouterOutputs } from '../../utils/trpc';
 import { trpc } from '../../utils/trpc';
-import DateCard from '../meeting/date-card';
+import DateCardDetails from '../meeting/date-card-details';
 import { Button, Loading } from '../ui';
 
 dayjs.extend(relativeTime);
@@ -215,60 +215,38 @@ export function MeetingDetailPage({ adminView, meeting, isLoading }: Props) {
                         return b.votes[Answer.YES] - a.votes[Answer.YES];
                       })
                       .slice(0, 5)
-                      .map((appointment) => {
-                        const start = new Date(appointment.value.split('/')[0] || '');
-                        const end = new Date(appointment.value.split('/')[1] || '');
-                        return (
-                          <div
-                            key={appointment.id}
-                            className="shrink-0 space-y-3 py-3 text-center"
-                            style={{ width: columnWidth }}
-                          >
-                            {/* TODO: Move DateCard + Annotation into its own component */}
-                            <DateCard date={start} />
-                            <div>
-                              <div className="relative -mr-2 inline-block pr-2 text-right text-xs font-semibold after:absolute after:top-2 after:right-0 after:h-4 after:w-1 after:border-t after:border-r after:border-b after:border-slate-300 after:content-['']">
-                                <div>{start.toLocaleTimeString()}</div>
-                                <div className="text-slate-400">{end.toLocaleTimeString()}</div>
+                      .map((appointment) => (
+                        <div
+                          key={appointment.id}
+                          className="shrink-0 space-y-3 py-3 text-center"
+                          style={{ width: columnWidth }}
+                        >
+                          <DateCardDetails value={appointment.value} />
+                          {Object.values(Answer).map((answer, i) => {
+                            return (
+                              <div key={i} className="flex items-center justify-around px-4">
+                                <AnswerIcon answer={answer} />
+                                {appointment.votes[answer]}
                               </div>
-                            </div>
-                            {Object.values(Answer).map((answer, i) => {
-                              return (
-                                <div key={i} className="flex items-center justify-around px-4">
-                                  <AnswerIcon answer={answer} />
-                                  {appointment.votes[answer]}
-                                </div>
-                              );
-                            })}
-                          </div>
-                        );
-                      })}
+                            );
+                          })}
+                        </div>
+                      ))}
                   </div>
                 </div>
               ))}
 
             <div className="card mt-4 overflow-x-auto p-4">
               <div className="flex py-2 pl-4 pr-2 font-medium" style={{ marginLeft: barWidth }}>
-                {sortedAppointments.map((appointment) => {
-                  const start = new Date(appointment.value.split('/')[0] || '');
-                  const end = new Date(appointment.value.split('/')[1] || '');
-                  return (
-                    <div
-                      key={appointment.id}
-                      className="shrink-0 space-y-3 py-3 text-center"
-                      style={{ width: columnWidth }}
-                    >
-                      {/* TODO: Replace this with a better card */}
-                      <DateCard date={start} />
-                      <div>
-                        <div className="relative -mr-2 inline-block pr-2 text-right text-xs font-semibold after:absolute after:top-2 after:right-0 after:h-4 after:w-1 after:border-t after:border-r after:border-b after:border-slate-300 after:content-['']">
-                          <div>{start.toLocaleTimeString()}</div>
-                          <div className="text-slate-400">{end.toLocaleTimeString()}</div>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
+                {sortedAppointments.map((appointment) => (
+                  <div
+                    key={appointment.id}
+                    className="shrink-0 space-y-3 py-3 text-center"
+                    style={{ width: columnWidth }}
+                  >
+                    <DateCardDetails value={appointment.value} />
+                  </div>
+                ))}
               </div>
               <div className="flex items-center py-2 pl-4 pr-2 font-medium">
                 <div className="flex h-full shrink-0 self-end" style={{ width: barWidth }}>
