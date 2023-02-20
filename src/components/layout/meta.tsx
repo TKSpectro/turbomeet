a0c3520e96256dd1961f3630529dd00329b568e9
Tom Kaeppler <tomkaeppler@web.de>
2023-02-20 22:46:21 +0100

    improve meta

--- src/components/layout/meta.tsx
+++ src/components/layout/meta.tsx
@@ -7,26 +7,20 @@ export function Meta() {
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-      <meta name="description" content="Plan your meetings efficiently" />
+      <meta name="description" content="Your meeting planner, turbocharged" />
 
-      <meta property="og:url" content="https://turbomeet-eight.vercel.app" />
+      <meta property="og:url" content="https://turbomeet.vercel.app" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="turbomeet" key="ogTitle" />
-      <meta property="og:description" content="Your money management software" />
-      <meta
-        property="og:image"
-        content="https://turbomeet-eight.vercel.app/images/turbomeet-logo.png"
-      />
+      <meta property="og:description" content="Your meeting planner, turbocharged" />
+      <meta property="og:image" content="https://turbomeet.vercel.app/images/turbomeet.png" />
 
       <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="turbomeet-eight.vercel.app" />
-      <meta property="twitter:url" content="https://turbomeet-eight.vercel.app" />
+      <meta property="twitter:domain" content="turbomeet.vercel.app" />
+      <meta property="twitter:url" content="https://turbomeet.vercel.app" />
       <meta name="twitter:title" content="turbomeet" />
-      <meta name="twitter:description" content="Plan your meetings efficiently" />
-      <meta
-        name="twitter:image"
-        content="https://turbomeet-eight.vercel.app/images/turbomeet-logo.png"
-      />
+      <meta name="twitter:description" content="Your meeting planner, turbocharged" />
+      <meta name="twitter:image" content="https://turbomeet.vercel.app/images/turbomeet.png" />
     </Head>
   );
 }
